ad3c65a15eba9f26c8f0f30f739cf22bdd84e405
Daniel Ortiz Bedoya <danbedoya2001@gmail.com>
2026-09-12 15:03:39 -0500

    feat: implementacion de arquitectura backend

---
+++ backend/config/db.py
@@ -0,0 +1,10 @@
+POSTGRES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tutorias_ecci',
+        'USER': 'tutorias_user',
+        'PASSWORD': 'change_me_secure_password',
+        'HOST': 'db',
+        'PORT': '5432',
+    }
+}
