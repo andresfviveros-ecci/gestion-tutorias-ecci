e5978d43385b1286199a04009c20987567de8a03
Daniel Ortiz Bedoya <danbedoya2001@gmail.com>
2026-09-19 16:56:48 -0500

    feat: se implementa endpoint para publicar la disponibilidad del tutor.

--- backend/apps/disponibilidad/models.py
+++ backend/apps/disponibilidad/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.conf import settings
+from backend.apps.disponibilidad.choices import MODALIDAD_CHOICES
+from backend.apps.models import BaseModel
 
-# Create your models here.
+class BloqueDisponibilidad(BaseModel):
+    docente = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,related_name='disponibilidades')
+    fecha = models.DateField(verbose_name='fecha')
+    hora_inicio = models.TimeField(verbose_name = 'hora_inicio')
+    hora_fin = models.TimeField(verbose_name='hora_find')
+    modalidad = models.CharField(max_length=11,choices=MODALIDAD_CHOICES,verbose_name='modalidad')
+
+    def __str__(self):
+        return f'{self.fecha} - {self.hora_inicio}'
+
+
+    class Meta:
+        verbose_name = 'Bloque de disponibilidad'
+        verbose_name_plural = 'Bloques de disponibilidad'
+        ordering = ['id']
+
+    
--- backend/apps/disponibilidad/views.py
+++ backend/apps/disponibilidad/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import status, views
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from .serializers import BloqueDisponibilidadSerializer
 
-# Create your views here.
+class BloqueDisponibilidadView(views.APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self,request,format=None):
+        serializer = BloqueDisponibilidadSerializer(data = request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(docente=request.user)
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
