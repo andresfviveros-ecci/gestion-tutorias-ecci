d9c03611641ae655eef8e347246203efbb067b99
Daniel Ortiz Bedoya <danbedoya2001@gmail.com>
2026-09-20 14:59:36 -0500

    feat: se implementa endpoint para generar token de recuperación y enviar correo electrónico.

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from config.db import POSTGRES
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -109,6 +110,14 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# CONFIGURACION DE VARIABLES PARA ENVIOS DE CORREO
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
